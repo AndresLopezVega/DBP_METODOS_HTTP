8ec9626eda08b9f14a417f33d388006e29d4e49b
Andres Lopez <andres.lopez@utec.edu.pe>
2023-08-25 17:48:15 -0500

    se añadio GET ID en metodos_http.py

--- metodos_http.py
+++ metodos_http.py
@@ -17,6 +17,19 @@ def obtener_info_animes():
         info += "Géneros: {}<br><br>".format(", ".join(anime["generos"]))
     return info
 
+def obtener_info_animes_id(ID):
+    animes = cargar_animes()
+    info = ""
+    for anime in animes:
+        if anime["id"]==ID:
+            info += "Título: {}<br>".format(anime["titulo"])
+            info += "Puntaje: {}<br>".format(anime["puntaje"])
+            info += "Tipo: {}<br>".format(anime["tipo"])
+            info += "Temporada: {}<br>".format(anime["season"])
+            info += "Géneros: {}<br><br>".format(", ".join(anime["generos"]))
+   
+    return info
+
 
 app = Flask(__name__)
 
@@ -25,6 +38,22 @@ app = Flask(__name__)
 def animes():
     info = obtener_info_animes()
     return info
+
+"""METODO GET ID"""
+
+@app.route("/anime/<int:id>",methods=["GET"])
+
+def anime_id(id):
+    info_ID=obtener_info_animes_id(id)
+    return info_ID
+
+
+
+"""METODO POST"""
+
+"""METODO PUT"""
+
+"METODO DELETE"
 
 if __name__ == "__main__":
     app.run()
